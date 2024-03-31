bc746e4eefc3305533a8d940759fec4c465a2ec6
shruthika21 <160160832+shruthika21@users.noreply.github.com>
2024-03-31 19:58:41 +0530

    main.py

--- main.py
+++ main.py
@@ -1,34 +1,12 @@
-import calendar, os, time
+import calendar
 
-print("\033[34mCALENDAR APP \033[0m")
-print("\033[30m©2024 Shruthi Programming Project \033[0m")
-time.sleep(4)
-os.system("clear")
+def display_calendar(year, month):
+    cal = calendar.month(year, month)
+    print(cal)
 
-def print_calendar(year, month):
-    print(calendar.month(year, month))
+# Input year and month
+year = int(input("Enter year: "))
+month = int(input("Enter month: "))
 
-while True:
-    os.system("clear")
-    year = int(input("Enter the year: "))
-    month = int(input("Enter the month (1-12): "))
-    if month > 12 or month < 1 :
-        print("\033[41mMonth Not Available. Please Try Again \033[0m")
-        time.sleep(2)
-        os.system("clear")
-        continue
-    time.sleep(1)
-    os.system("clear")
-    print_calendar(year, month)
-    
-    while True:
-        print()
-        answer = input("Check another date? (y/n): ").lower().strip()
-        if answer == 'y':
-            break
-        elif answer == 'n':
-            os.system("clear")
-            print("Thakyou For Using Calendar App")
-            exit()
-        else:
-            print("Please enter 'y' or 'n'.")
+# Display calendar
+display_calendar(year, month)
